3722316e86c1e7fa15e29439cb85de40519e9fb6
Alex <alexftf_xd@hotmail.com>
2025-12-18 14:36:40 -0300

    Agrego Tareas y Creo Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import './App.css'
 
+// Tareas/Componentes por crear:
+// Crear Habitos
+// Listar Habitos
+// Marcar habitos como completados
+// Eliminar Habitos
+// Mostrar total, completados, pendientes
+
+
 function App() {
 
   return (
